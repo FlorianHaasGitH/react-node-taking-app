94365fa584c688bbf4a093bc0e24c53b04836ef9
Florian-Haas <fhaasbusiness@gmail.com>
2025-04-05 14:46:07 +0200

    Finish form structure

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,4 +1,6 @@
-import { Form, Row, Col, Stack } from "react-bootstrap";
+import { Form, Row, Col, Stack, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CreatableReactSelect from "react-select/creatable";
 
 export default function NoteForm() {
   return (
@@ -11,13 +13,25 @@ export default function NoteForm() {
               <Form.Control required />
             </Form.Group>
           </Col>
-          <Col>
-            <Form.Group controlId="title">
-              <Form.Label>Title</Form.Label>
-              <Form.Control required />
-            </Form.Group>
-          </Col>
         </Row>
+        <Form.Group controlId="tags">
+          <Form.Label>Tags</Form.Label>
+          <CreatableReactSelect isMulti />
+        </Form.Group>
+        <Form.Group controlId="mardown">
+          <Form.Label>Body</Form.Label>
+          <Form.Control required as="textarea" rows={15} />
+        </Form.Group>
+        <Stack direction="horizontal" gap={2} className="justify-content-end">
+          <Button type="submit" variant="primary">
+            Save
+          </Button>
+          <Link to="..">
+            <Button type="button" variant="outline-secondary">
+              Cancel
+            </Button>
+          </Link>
+        </Stack>
       </Stack>
     </Form>
   );
