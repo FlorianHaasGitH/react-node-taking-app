c3760b6522cd6f9d5bcf132e35918e2ea4465e66
Florian-Haas <fhaasbusiness@gmail.com>
2025-04-05 18:53:09 +0200

    Add multi-select tag support with CreatableReactSelect in note form

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -3,6 +3,8 @@ import { Form, Row, Col, Stack, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
 import { NoteData } from "./App";
+import { useState } from "react";
+import { Tag } from "./App";
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
@@ -11,9 +13,16 @@ type NoteFormProps = {
 export default function NoteForm({ onSubmit }: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
 
   function handleSubmit(e: FormEvent) {
     e.preventDefault();
+
+    onSubmit({
+      title: titleRef.current!.value,
+      markdown: markdownRef.current!.value,
+      tags: [],
+    });
   }
 
   return (
@@ -29,7 +38,19 @@ export default function NoteForm({ onSubmit }: NoteFormProps) {
         </Row>
         <Form.Group controlId="tags">
           <Form.Label>Tags</Form.Label>
-          <CreatableReactSelect isMulti />
+          <CreatableReactSelect
+            value={selectedTags.map((tag) => {
+              return { label: tag.label, value: tag.id };
+            })}
+            onChange={(tags) => {
+              setSelectedTags(
+                tags.map((tag) => {
+                  return { label: tag.label, id: tag.value };
+                })
+              );
+            }}
+            isMulti
+          />
         </Form.Group>
         <Form.Group controlId="mardown">
           <Form.Label>Body</Form.Label>
