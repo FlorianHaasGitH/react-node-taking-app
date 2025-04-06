230271345f1620e2a1586607bb0617df22bfc722
Florian-Haas <fhaasbusiness@gmail.com>
2025-04-06 16:37:39 +0200

    Create NoteList component

--- src/NoteForm.tsx
+++ src/NoteForm.tsx
@@ -1,24 +1,27 @@
-import { FormEvent, useRef } from "react";
-import { Form, Row, Col, Stack, Button } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { FormEvent, useRef, useState } from "react";
+import { Button, Col, Form, Row, Stack } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
 import CreatableReactSelect from "react-select/creatable";
-import { NoteData } from "./App";
-import { useState } from "react";
-import { Tag } from "./App";
+import { NoteData, Tag } from "./App";
 import { v4 as uuidV4 } from "uuid";
-import onAddTag from "./App";
-import { useNavigate } from "react-router-dom";
 
 type NoteFormProps = {
   onSubmit: (data: NoteData) => void;
   onAddTag: (tag: Tag) => void;
   availableTags: Tag[];
-};
+} & Partial<NoteData>;
 
-export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
+export function NoteForm({
+  onSubmit,
+  onAddTag,
+  availableTags,
+  title = "",
+  markdown = "",
+  tags = [],
+}: NoteFormProps) {
   const titleRef = useRef<HTMLInputElement>(null);
   const markdownRef = useRef<HTMLTextAreaElement>(null);
-  const [selectedTags, setSelectedTags] = useState<Tag[]>([]);
+  const [selectedTags, setSelectedTags] = useState<Tag[]>(tags);
   const navigate = useNavigate();
 
   function handleSubmit(e: FormEvent) {
@@ -40,37 +43,45 @@ export function NoteForm({ onSubmit, onAddTag, availableTags }: NoteFormProps) {
           <Col>
             <Form.Group controlId="title">
               <Form.Label>Title</Form.Label>
-              <Form.Control required ref={titleRef} />
+              <Form.Control ref={titleRef} required defaultValue={title} />
+            </Form.Group>
+          </Col>
+          <Col>
+            <Form.Group controlId="tags">
+              <Form.Label>Tags</Form.Label>
+              <CreatableReactSelect
+                onCreateOption={(label) => {
+                  const newTag = { id: uuidV4(), label };
+                  onAddTag(newTag);
+                  setSelectedTags((prev) => [...prev, newTag]);
+                }}
+                value={selectedTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                options={availableTags.map((tag) => {
+                  return { label: tag.label, value: tag.id };
+                })}
+                onChange={(tags) => {
+                  setSelectedTags(
+                    tags.map((tag) => {
+                      return { label: tag.label, id: tag.value };
+                    })
+                  );
+                }}
+                isMulti
+              />
             </Form.Group>
           </Col>
         </Row>
-        <Form.Group controlId="tags">
-          <Form.Label>Tags</Form.Label>
-          <CreatableReactSelect
-            onCreateOption={(label: string) => {
-              const newTag: Tag = { id: uuidV4(), label };
-              onAddTag(newTag);
-              setSelectedTags((prev) => [...prev, newTag]);
-            }}
-            value={selectedTags.map((tag) => {
-              return { label: tag.label, value: tag.id };
-            })}
-            options={availableTags.map((tag) => {
-              return { label: tag.label, value: tag.id };
-            })}
-            onChange={(tags) => {
-              setSelectedTags(
-                tags.map((tag) => {
-                  return { label: tag.label, id: tag.value };
-                })
-              );
-            }}
-            isMulti
-          />
-        </Form.Group>
-        <Form.Group controlId="mardown">
+        <Form.Group controlId="markdown">
           <Form.Label>Body</Form.Label>
-          <Form.Control required as="textarea" ref={markdownRef} rows={15} />
+          <Form.Control
+            defaultValue={markdown}
+            required
+            as="textarea"
+            ref={markdownRef}
+            rows={15}
+          />
         </Form.Group>
         <Stack direction="horizontal" gap={2} className="justify-content-end">
           <Button type="submit" variant="primary">
